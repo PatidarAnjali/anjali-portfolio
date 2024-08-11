7e1153daf888ec09a8bb7a83ca9af03512246a8d
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-11 17:21:43 -0400

    frontend + backend

--- frontend/src/app/projects/projects.component.ts
+++ frontend/src/app/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import {MatCardModule} from '@angular/material/card';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MatCardModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })

c84a26a80c9e3fda1cdb8d5ed4ca530646d13934
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-11 10:32:04 -0400

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'anjali-portfolio';
+  isHandset: boolean = false;
+
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([Breakpoints.Handset])
+      .subscribe(result => {
+        this.isHandset = result.matches;
+        if (!this.isHandset) {
+          this.closeSidenav();
+        }
+      });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    if (window.innerWidth > 600) {
+      this.closeSidenav();
+    }
+  }
+
+  closeSidenav() {
+    if (this.sidenav && this.sidenav.opened) {
+      this.sidenav.close();
+    }
+  }
 }

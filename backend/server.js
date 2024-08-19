930ae31cbc878421e036da35cfa10d746e053067
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-19 19:57:32 -0400

    server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const projectRoutes = require('./routes/projects');
 const helmet = require('helmet');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json()); // middleware
 app.use(helmet());

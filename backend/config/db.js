c15a614c0d6bf35a2d85e6c08d6cf476fdea23f2
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-19 20:02:28 -0400

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(process.env.MONGO_URL, {});
         console.log("MongoDB connection success!");
     } catch(error) {
         console.error("MongoDB connection fail :(", error);

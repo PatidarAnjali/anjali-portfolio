371d1c7226957a8c602f1d8e27549a091c13121f
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-20 19:19:35 -0400

    server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors({
 const connectDB = require("./config/db");
 connectDB();
 
-app.use('/projects', projectRoutes);
+app.use('/api', projectRoutes);
 
 app.listen(port, () => {
   console.log(`Anjali's Portfolio is listening on port ${port}`)

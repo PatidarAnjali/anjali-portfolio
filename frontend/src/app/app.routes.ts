bf0a653e08c63ed88ab75e4b8e0c338c0434c989
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-20 20:02:17 -0400

    changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown paths to home
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "about", component: AboutComponent },
+    { path: "projects", component: ProjectsComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Redirect unknown paths to home
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {}
+

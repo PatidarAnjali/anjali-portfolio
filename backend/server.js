eaf915bdb47f01f536838b84df3165011e4f1d81
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-24 12:14:30 -0400

    new

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const projectRoutes = require('./routes/projects');
 const helmet = require('helmet');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,6 +20,19 @@ connectDB();
 
 app.use('/api', projectRoutes);
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  })
+} else {
+  app.get("/", (req, res) => {
+    res.json({
+      message: "API running..."
+    });
+  })
+};
+
 app.listen(port, () => {
   console.log(`Anjali's Portfolio is listening on port ${port}`)
-})
+});

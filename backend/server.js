3ae94342a75f28fd113b3e128937a87d8a05ff0b
Anjali Patidar <anjali.ckpatidar@gmail.com>
2024-08-24 14:26:50 -0400

    server

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,10 @@ app.use('/api', projectRoutes);
 
 // Serve static files from Angular build
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "dist"))); 
-  
+  app.use(express.static(path.join(__dirname, "../frontend/dist/anjali-portfolio")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/anjali-portfolio", "index.html"));
   });
 } else {
   app.use(express.static(path.join(__dirname, "app"))); // Serve app folder in development
